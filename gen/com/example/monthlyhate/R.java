cd2d731ff58d4b9830885784ffc3ab014101fa80
Stefan Slaveykov <stefan@emerchantpay.com>
2013-01-05 16:12:29 +0200

    Background, datepicker, button and some code

--- gen/com/example/monthlyhate/R.java
+++ gen/com/example/monthlyhate/R.java
@@ -11,10 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btnChangeDate=0x7f070002;
+        public static final int datePicker=0x7f070001;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,8 +28,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int enter_your_desired_date_and_be_prepared_to_face_the_monthly_hate=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int select=0x7f040004;
     }
     public static final class style {
         /** 
--- src/com/example/monthlyhate/MainActivity.java
+++ src/com/example/monthlyhate/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.monthlyhate;
 
+import java.util.Calendar;
+import android.content.Intent;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.app.Dialog;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private DatePicker dp;
+	private Button select;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addBtnListener();
 	}
 
 	@Override
@@ -17,6 +31,35 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	private void getSelectedDate()
+	{
+		dp = (DatePicker) findViewById(R.id.datePicker);
+	}
+	
+	private void addDates()
+	{
+		Calendar cal = Calendar.getInstance();              
+		Intent intent = new Intent(Intent.ACTION_EDIT);
+		intent.setType("vnd.android.cursor.item/event");
+		intent.putExtra("beginTime", cal.getTimeInMillis());
+		intent.putExtra("allDay", true);
+		intent.putExtra("title", "Monthly Hate");
+		startActivity(intent);
+	}
+	
+	public void addBtnListener()
+	{
+		select = (Button) findViewById(R.id.btnChangeDate);
+		 
+		select.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				addDates();
+			}
+ 
+		});
 	}
 
 }
