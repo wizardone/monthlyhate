3aaebf91d02e14b8d7f480201c287e26cd57a460
Stefan Slaveykov <stefan@emerchantpay.com>
2013-02-09 16:21:08 +0200

    Added second activity

--- src/com/example/monthlyhate/MainActivity.java
+++ src/com/example/monthlyhate/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.monthlyhate;
 
-import java.util.Calendar;
-import android.content.Intent;
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.app.Dialog;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
---
+++ src/com/example/monthlyhate/ShowHate.java
@@ -0,0 +1,13 @@
+package com.example.monthlyhate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowHate extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
